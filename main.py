152315bbbc7f521052513d46de6367019fd35b44
LeadSeason <pauli.tiittanen113@gmail.com>
2021-11-23 14:38:46 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import traceback
+import argparse
+
+
+class pcolor:
+    OK = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    NORMAL = '\033[0m'
+
+
+def log(arg="", color=pcolor.NORMAL, end=""):
+    print(f"{color}{arg}{pcolor.NORMAL}", end=end)
+
+
+def logln(arg="", color=pcolor.NORMAL, end="\n"):
+    print(f"{color}{arg}{pcolor.NORMAL}", end=end)
+
+
+try:
+    import requests
+except ImportError:
+    traceback.print_exc()
+    logln("requests is needed to run this script\nhttps://pypi.org/project/requests/", pcolor.FAIL)
+    sys.exit(1)
+
+try:
+    import yaml
+except ImportError:
+    logln("PyYaml is required to parse configuration\nhttps://pypi.org/project/PyYAML/", pcolor.FAIL)
+    sys.exit(1)
+
+try:
+    import keyring
+except ImportError:
+    logln("keyring is required to store key securly\nhttps://pypi.org/project/keyring/", pcolor.WARNING)
+    sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--upload", help="Upload image to host")
+    parser.add_argument("-S", "--set-key", help="Sets key intothe keyring") 
+    parser.add_argument("-c", "--config", help="point to the conf file will default to ~/.config/fsuploader.yaml")
+    args = parser.parse_args()
+
+    if keyring:
+        keyring.
+
+
+if __name__ == "__main__":
+    main()
